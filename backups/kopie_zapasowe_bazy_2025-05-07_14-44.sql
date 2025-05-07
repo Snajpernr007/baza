7280184e59a95fd41d2d1500d1ba362545a8fdbc
GrobROBmini <grobrob321@gmail.com>
2025-05-07 14:46:59 +0200

    pliki szkielet

--- backups/kopie_zapasowe_bazy_2025-05-07_14-44.sql
+++ backups/kopie_zapasowe_bazy_2025-05-07_14-44.sql
@@ -85,6 +85,7 @@ CREATE TABLE `tasma` (
 CREATE TABLE `szablon_profile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa` varchar(255) NOT NULL,
+  `waga_w_kg_na_1_metr` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -161,7 +162,7 @@ INSERT INTO `tasma` (id, data_z_etykiety_na_kregu, grubosc, szerokosc, waga_kreg
 INSERT INTO `tasma` (id, data_z_etykiety_na_kregu, grubosc, szerokosc, waga_kregu, waga_kregu_na_stanie, nr_etykieta_paletowa, nr_z_etykiety_na_kregu, lokalizacja_id, nr_faktury_dostawcy, data_dostawy, pracownik_id, dostawca_id, szablon_id, Data_do_usuwania) VALUES ('54', '0576-06-07', '0.00', '0.00', '765.00', '765.00', '756', '756', '1', '576', '0006-05-07', '1', '5', '4', '2026-04-28');
 
 -- Dane z tabeli `szablon_profile`
-INSERT INTO `szablon_profile` (id, nazwa) VALUES ('3', '1235');
+INSERT INTO `szablon_profile` (id, nazwa, waga_w_kg_na_1_metr) VALUES ('3', '1235', '0.00');
 
 -- Dane z tabeli `profil`
 INSERT INTO `profil` (id, id_tasmy, data_produkcji, godz_min_rozpoczecia, godz_min_zakonczenia, zwrot_na_magazyn_kg, id_szablon_profile, nazwa_klienta_nr_zlecenia_PRODIO, ilosc, ilosc_na_stanie, id_dlugosci, id_pracownika, Imie_nazwisko_pracownika, Data_do_usuwania) VALUES ('65', '52', '2025-04-28', '19:16:29', '19:16:44', '3.00', '3', '3422', '243', '243', '1', '1', '432', '2026-04-28');
